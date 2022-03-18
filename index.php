7e250cf29a15823458bc171a0d61b83192e4febb
cosmonet-fr <stephane.billois@hotmail.com>
2022-03-18 15:42:56 +0100

    Add style

--- index.php
+++ index.php
@@ -4,20 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
     <title>Livres</title>
 </head>
 <body>
-    <h1>Liste des livres</h1>
-    <ul>
-        <?php
-        $scandir = scandir("./library");
-        foreach($scandir as $fichier){
-            if(preg_match("#\.(epub)$#",strtolower($fichier))){
-                echo "<li><a href=\"library/$fichier\">$fichier</a></li>";
+    <section>
+        <h1>Liste des livres</h1>
+        <ul>
+            <?php
+            $scandir = scandir("./library");
+            foreach($scandir as $fichier){
+                if(preg_match("#\.(epub)$#",strtolower($fichier))){
+                    echo "<li><a href=\"library/$fichier\">$fichier</a></li>";
+                }
             }
-        }
-        ?>
-    </ul>
+            ?>
+        </ul>
+
+    </section>
     
 </body>
 </html>
